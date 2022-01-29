bf6467ee911965f9dc6f7891c4e57ff65c6168a8
l1ghbe <ee1zet25@gmail.com>
2022-01-29 16:53:22 +0300

    added footer and about section

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,6 +2,14 @@ const burger = () => {
     const burger = document.querySelector('.menu-burger');
     const menu = document.querySelector('.header-menu');
     const menuLinks = document.querySelectorAll('.header-menu__item');
+    const navLinks = document.querySelectorAll('.header-menu__link');
+    const contactsBtn = document.getElementById('contacts-btn')
+    const aboutBtn = document.getElementById('about-btn')
+    const mainBtn = document.getElementById('main-btn')
+    // const contacts = document.getElementById('contacts')
+    const about = document.getElementById('about')
+
+
 
     burger.addEventListener('click', () => {
         burger.classList.toggle('active')
@@ -18,6 +26,27 @@ const burger = () => {
                 link.classList.remove('active')
             })
         }
+    })
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.body.removeAttribute('style')
+            menu.classList.remove('active')
+            menuLinks.forEach(link => {
+                link.classList.remove('active')
+            })
+            burger.classList.remove('active')
+        } )
+    })
+
+    contactsBtn.addEventListener('click', () => {
+        window.scrollBy(0, window.innerHeight)
+    })
+    mainBtn.addEventListener('click', () => {
+        window.scrollBy(0, window.innerHeight)
+    })
+    aboutBtn.addEventListener('click', () => {
+        about.scrollIntoView(false)
     })
      
 }
